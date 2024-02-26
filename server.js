f57ffc45ec8451740f0d3ff4f30792f1fa955626
King Robert Kehinde <robert.k@techie.com>
2024-02-26 15:25:08 +0000

    first commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const http = require("http");
+
+// MY SERVER PORT
+const PORT = 3000;
+
+// CREATED MY SERVER USING THE HTTP createServer Method
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/html");
+  res.end("<h1>Welcome To King Robert 30 Days Of NODEJS</h1>");
+});
+
+// START THE SERVER & LISTEN ON PORT 3000
+server.listen(PORT, () => {
+  console.log(`Server running... http://localhost:${PORT}/`);
+});
